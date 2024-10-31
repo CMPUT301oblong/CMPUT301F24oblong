6bc030a06ae404a933801a9147f2064514ac6870
Kayda Seutter <kmseutter@gmail.com>
2024-10-31 15:06:43 -0600

    added .java activity files for kayda screens and modified .gitignore to allow oblongApp/app files

---
+++ oblongApp/app/src/main/java/com/example/oblong/organizer_create_notification_activity.java
@@ -0,0 +1,19 @@
+package com.example.oblong;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class organizer_create_notification_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.organizer_create_notification);
+    }
+}
