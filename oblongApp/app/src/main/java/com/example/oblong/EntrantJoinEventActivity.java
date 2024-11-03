a33510a852d846ed323f862313a010916a7cd549
Kyle Campbell <kmcampb1@ualberta.ca>
2024-11-03 13:06:03 -0700

    can now add users to an event in the database, waiting for QR code to be complete to specify which event

--- oblongApp/app/src/main/java/com/example/oblong/EntrantJoinEventActivity.java
+++ oblongApp/app/src/main/java/com/example/oblong/EntrantJoinEventActivity.java
@@ -1,12 +1,19 @@
 package com.example.oblong;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EntrantJoinEventActivity extends AppCompatActivity {
 
@@ -19,6 +26,27 @@ public class EntrantJoinEventActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Initialize
+        Database db = new Database();
+        Button joinButton = findViewById(R.id.join_event_join_button);
+        String event_id = "TEST2";
+
+        // join button listener
+        joinButton.setOnClickListener(v -> {
+            // get current user (asynchronous)
+            db.getCurrentUser(userId -> {
+                // once userid is retrieved add participant
+                String participantId = userId + event_id;
+                String event = "event_id"; // to be replaced with the event id passed in thru intent from QR scanner
+                String location = "[0° N, 0° E]"; // ?
+                String status = "attending";
+
+                // add user as a participant
+                db.addParticipant(participantId, userId, event, location, status);
+                startActivity(new Intent(EntrantJoinEventActivity.this, EntrantEventList.class));
+            });
         });
     }
 }
